6a1f0ae00e6c1679ad4eb1f295371d18310d0569
newrodrigo <rodrigocerqueira@outlook.com>
2023-12-27 21:02:18 -0300

    About

--- dsrotas/src/App.tsx
+++ dsrotas/src/App.tsx
@@ -1,18 +1,21 @@
 import "./index.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Home from "./routes/Home";
 import HomeBody from "./routes/Home/HomeBody";
+import About from "./routes/Home/About";
 
 export default function App() {
   return (
     <>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />}>
-          <Route index element={<HomeBody />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}>
+            <Route index element={<Navigate to="/home" />} />
+            <Route path="home" element={<HomeBody />} />
+            <Route path="about" element={<About />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
--- dsrotas/src/components/Header/index.tsx
+++ dsrotas/src/components/Header/index.tsx
@@ -1,17 +1,34 @@
 import "./styles.css";
 import homeIcon from "../../assets/home.svg";
 import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export default function Header() {
   return (
     <header>
       <nav className="container">
         <div className="navbar-left">
-          <div className="active-menu-item">
-            <Link to="/">Início</Link>
+          <div>
+            <NavLink
+              to="/home"
+              className={({ isActive }) =>
+                isActive ? "active-menu-item " : ""
+              }
+            >
+              Início
+            </NavLink>
           </div>
           <div>Produtos</div>
-          <div>Sobre nós</div>
+          <div>
+            <NavLink
+              to="/about"
+              className={({ isActive }) =>
+                isActive ? "active-menu-item " : ""
+              }
+            >
+              Sobre nós
+            </NavLink>
+          </div>
         </div>
         <div className="navbar-icon">
           <Link to="/">
